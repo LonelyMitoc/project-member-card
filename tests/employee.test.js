1fb42586ea3b178e86588285102d33a5c0e02010
Triglyceride <jamesmatsu@gmail.com>
2022-10-26 14:04:06 -0700

    finish employee test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,6 +1,3 @@
-const { exportAllDeclaration } = require('@babel/types');
-const { it } = require('node:test');
-const { describe } = require('yargs');
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
@@ -14,13 +11,84 @@ describe('Employee', () => {
         })
     })
 
-    describe('getName', () => {
+    // Testing each 'get' method in the constructor for the correct inputs
+    describe('Correct Input', () => {
         it(`getName method should return 'name' input`, () => {
             const obj = new Employee('jane', 123, 'example@gmail.com');
 
             expect(obj.getName()).toBe('jane');
         })
 
-        it(``)
+        it(`getId method should return 'id' input`, () => {
+            const obj = new Employee('jane', 123, 'example@gmail.com');
+
+            expect(obj.getId()).toBe(123);
+        })
+
+        it(`getEmail method should return 'email' input`, () => {
+            const obj = new Employee('jane', 123, 'example@gmail.com');
+
+            expect(obj.getId()).toBe(123);
+        })
+
+        it(`getRole method should return 'Employee'`, () => {
+            const obj = new Employee('jane', 123, 'example@gmail.com');
+
+            expect(obj.getRole()).toBe('Employee');
+        })
     })
+
+    // Testing each incorrect input
+    describe('Incorrect Input'), () => {
+        // For getName
+        it(`Error if 'name' input is not a string`, () => {
+
+            const testInput = () => new Employee(1, 123, 'example@gmail.com').getName();
+            const err = new Error(`'name' input value must be a string value`);
+            
+            expect(testInput).toThrowError(err);
+        })
+
+        it(`Error if 'name' input is empty`, () => {
+
+            const testInput = () => new Employee('', 123, 'example@gmail.com').getName();
+            const err = new Error(`'name' input value must be a string value`);
+            
+            expect(testInput).toThrowError(err);
+        })
+
+        //For getId
+        it(`Error if 'id' input is not a number`, () => {
+
+            const testInput = () => new Employee('jane', '123', 'example@gmail.com').getId();
+            const err = new Error(`'id' input value must be a positive number`);
+            
+            expect(testInput).toThrowError(err);
+        })
+
+        it(`Error if 'id' input is a negative number`, () => {
+
+            const testInput = () => new Employee('jane', -987, 'example@gmail.com').getId();
+            const err = new Error(`'id' input value must be a positive number`);
+            
+            expect(testInput).toThrowError(err);
+        })
+
+        //For getEmail
+        it(`Error if 'email' input is not a string`, () => {
+
+            const testInput = () => new Employee('jane', 123, 456).getEmail();
+            const err = new Error(`'email' input value must be a string value`);
+            
+            expect(testInput).toThrowError(err);
+        })
+
+        it(`Error if 'email' input is empty`, () => {
+
+            const testInput = () => new Employee('jane', 123, '').getEmail();
+            const err = new Error(`'email' input value must be a string value`);
+            
+            expect(testInput).toThrowError(err);
+        })
+    }
 })
